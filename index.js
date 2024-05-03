2bbc0f8c0450faa24755bfef0cf78bd4c939bc96
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-05-03 21:10:18 +0530

    Add: added comments to all functions

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const fs = require("fs");
 
 const fileName = "sample.txt"
 
+/**************************************************************************************************************
+stringToArray function is used to generate an array of tasks from the tasks that are soted inside the txt file
+
+@param {String} fileName - the name of the file in which you have stored the tasks
+
+@returns {Array} An array of strings , where each sting is a task that was stored inside the txt file
+***************************************************************************************************************/
+
 const stringToArray = (fileName) => {
     const data = readFile(fileName);
     const result = JSON.stringify(data);
@@ -11,6 +19,14 @@ const stringToArray = (fileName) => {
 
     return filteredResult;
 }
+
+/**********************************************************************************************************************************
+overWriteDb function is used to overwrite all the data that currently exists in the file with the data that is generated inside the function
+@param {String} fileName - the name of the file which you want to overwrite
+@param {Array} arr - array of strings that contains the tasks
+
+@returns none
+************************************************************************************************************************************/
 
 const overWriteDb = (fileName, arr) => {
     arr.push("");
@@ -22,6 +38,13 @@ const overWriteDb = (fileName, arr) => {
     }
 }
 
+/**************************************************************************************************************
+appendFile function is used to add new tasks to the txt file 
+@param {String} fileName - the name of the file on which you want to add the task
+@param {String} dataToWrite - the task which you want to add to the file
+
+@returns none
+***************************************************************************************************************/
 const appendFile = (fileName,dataToWrite) => {
     try{
         fs.appendFileSync(fileName,`${dataToWrite}\n`);
@@ -30,6 +53,13 @@ const appendFile = (fileName,dataToWrite) => {
         console.log(error);
     }
 }
+
+/**************************************************************************************************************
+readFile function is used to get all the tasks that are stored in the txt file
+@param {String} fileName - the name of the file from which you want to read the task from
+
+@returns {String} return the tasks stored in the file
+***************************************************************************************************************/
 
 const readFile = (fileName) => {
     try{
@@ -40,6 +70,16 @@ const readFile = (fileName) => {
         return null;
     }
 }
+
+/**************************************************************************************************************
+markTaskAsComplete function is used to mark a task in the txt file as done
+@param {String} fileName - the name of the file on which you stored the tasks in
+@param {String} taskName - the task which you want to mark as done
+
+@returns none
+
+When a task is marked as done the task will get wraped in square braces ([])
+***************************************************************************************************************/
 
 const markTaskAsComplete = (fileName, taskName) => {
 
@@ -56,6 +96,15 @@ const markTaskAsComplete = (fileName, taskName) => {
     overWriteDb(fileName, mappedResult);
 
 }
+
+/**************************************************************************************************************
+deleteTask function is used to delete a task in the txt file 
+@param {String} fileName - the name of the file on which you stored the tasks in
+@param {String} taskName - the task which you want to delete from the file
+
+@returns none
+
+***************************************************************************************************************/
 
 const deleteTask = (fileName, taskName) => {
     const data = stringToArray(fileName);
